0923588551dff482a1f1a9516f0f7df6db5b3e99
keshen <hackersk@qq.com>
2017-09-25 15:20:42 +0800

    upd8: iframe agent page debug.

--- build/iframe-agent.build.js
+++ build/iframe-agent.build.js
@@ -1,11 +1,26 @@
 const fs = require('fs');
 const path = require('path');
 
+let debugMode = false;
+
+const args = process.argv.slice(2);
+args.forEach(function (val, index, array) {
+    console.log(`${index}: ${val}`);
+    switch (val) {
+    case '--debug':
+        debugMode = true;
+        break;
+    default:
+    }
+});
+
+const suffix = debugMode ? '-debug' : '';
+
 const child_process = require('child_process');
 child_process.execSync(path.resolve(process.cwd(), './node_modules/.bin/rollup')
-    + ' -c ./build/rollup-iframe-agent.config.js');
+    + ` -c ./build/rollup-iframe-agent${suffix}.config.js`);
 
-fs.writeFileSync(path.resolve(process.cwd(), './dist/iframe-agent.html'),
+fs.writeFileSync(path.resolve(process.cwd(), `./dist/iframe-agent${suffix}.html`),
     fs.readFileSync(path.resolve(process.cwd(), './src/iframe-agent.html'), 'utf8').replace(
         '/* iframe-agent.js */',
         fs.readFileSync(path.resolve(process.cwd(), './temp/iframe-agent.js'), 'utf8')
---
+++ build/rollup-iframe-agent-debug.config.js
@@ -0,0 +1,18 @@
+const uglify = require('rollup-plugin-uglify');
+
+module.exports = {
+    input: 'src/iframe-agent.js',
+    output: {
+        file: 'temp/iframe-agent.js',
+        format: 'iife',
+        name: 'iframeAgent'
+    },
+    plugins: [
+        // uglify({
+        //     mangle: {
+        //         reserved: ['HostWhitelist'],
+        //     },
+        //     ie8: true
+        // })
+    ]
+};
